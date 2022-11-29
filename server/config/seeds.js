dd849ea4f2cdd4bf5b4b7011ad757a8adcb30383
watssupp <106455345+watssupp@users.noreply.github.com>
2022-11-28 21:46:51 -0600

    Update seeds.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,5 +1,5 @@
 const db = require('./connection');
-const { User, Product} = require('../models');
+const { User, Product,Category} = require('../models');
 
 db.once('open', async () => {
     await Category.deleteMany();
