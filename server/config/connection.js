f443962ed8c74a2b1f980cb3a495e96c121a10d2
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-01 19:03:10 -0600

    added requires and module exports to schema index.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL || '', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/E-scentials', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
